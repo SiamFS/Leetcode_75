7c2040377dc7fc1cc07bae9fb6d8c913a4643c2f
SIAM FERDOUS <siamferdous1@gmail.com>
2025-11-26 02:03:51 +0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.58%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,18 +1,15 @@
 class Solution(object):
     def gcdOfStrings(self, str1, str2):
-        
-        # Check if gcd string is even possible
+
         if str1 + str2 != str2 + str1:
             return ""
         
         len1 = len(str1)
         len2 = len(str2)
-        
-        # Euclid GCD
+
         while len2 != 0:
             rem = len1 % len2
             len1 = len2
             len2 = rem
-        
-        # len1 is now gcd length
+
         return str1[:len1]
