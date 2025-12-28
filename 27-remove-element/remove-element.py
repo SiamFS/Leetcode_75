9e79d6ca4d09fddc2db0a31032a3f89fe6454887
SIAM FERDOUS <siamferdous1@gmail.com>
2025-12-28 14:11:49 +0600

    Time: 0 ms (100.00%) | Memory: 12.3 MB (81.08%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        index = 0
+        for i in nums:
+            if i != val:
+                nums[index] = i
+                index += 1
+        return index
+        
