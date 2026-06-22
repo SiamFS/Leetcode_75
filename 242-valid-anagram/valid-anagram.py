a05865dff35d8b1c7255e9d3b10f42faaaa35723
SIAM FERDOUS <siamferdous1@gmail.com>
2026-06-22 08:21:37 +0600

    Time: 15 ms (86.88%) | Memory: 12.6 MB (49.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,11 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s)==len(t):
-           return sorted(s)==sorted(t)
-        return False
-        
+        frq={}
+        for i in s:
+            frq[i]=frq.get(i,0)+1
+        for i in t:
+            frq[i]=frq.get(i,0)-1
+        for i in frq.values():
+             if i !=0:
+                return False
+        return True
