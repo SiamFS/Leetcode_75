7f0e80e18fdb960d1ce349b0f52325fcdefbe82a
SIAM FERDOUS <siamferdous1@gmail.com>
2026-06-24 17:10:45 +0600

    Time: 11 ms (26.81%) | Memory: 13.7 MB (19.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -2,8 +2,7 @@ class Solution(object):
     def moveZeroes(self, nums):
         j=0
         for i in range(len(nums)):
-            if nums[i]!=0: 
-                nums[i],nums[j]=nums[j],nums[i]
+            if nums[i]!=0:
+                nums[i], nums[j] = nums[j], nums[i]
                 j+=1
-                
-        
+        return nums
