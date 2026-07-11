c4c173f51d2e0a29be3f6ca0a06346a5dde9bb98
SIAM FERDOUS <siamferdous1@gmail.com>
2026-07-11 11:33:48 +0600

    Time: 32 ms (20.60%) | Memory: 26 MB (13.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-         dic={}
-         flag=False
-         for i in nums:
-            if i not in dic:
-                dic[i]=1
-            else: 
-                flag=True
-                return flag
-         return flag        
+        hashset=set()
+        for n in nums:
+            if n in hashset: 
+                return True
+            hashset.add(n)
+        return False       
