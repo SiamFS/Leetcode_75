4bfd168762a76cba74197e0c375f67272e33bfff
SIAM FERDOUS <siamferdous1@gmail.com>
2026-07-13 19:18:23 +0600

    Time: 12 ms (50.37%) | Memory: 13.3 MB (86.54%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def trap(self, height):
+        l,r=0,len(height)-1
+        leftmax,rightmax=height[l],height[r]
+        res=0
+        while l<r:
+            if leftmax<rightmax:
+                l+=1
+                leftmax=max(leftmax,height[l])
+                res+=leftmax-height[l]
+            else:
+                r-=1
+                rightmax=max(rightmax,height[r])
+                res+=rightmax-height[r]
+        return res
+        
