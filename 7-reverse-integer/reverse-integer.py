f64b190a491985ec4c6fdbcc122eb6ca20bb3046
SIAM FERDOUS <siamferdous1@gmail.com>
2026-08-02 22:19:31 +0600

    Time: 24 ms (16.83%) | Memory: 12.4 MB (51.95%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -2,13 +2,12 @@ class Solution(object):
 
     def reverse(self, x):
 
-        # Store the original sign
+      
         if x < 0:
             sign = -1
         else:
             sign = 1
 
-        # Convert the number to a positive value
         x = abs(x)
 
         result = 0
@@ -21,14 +20,12 @@ class Solution(object):
 
             x = x // 10
 
-        # Restore the sign
+
         result = result * sign
 
-        # Check the lower limit
         if result < -2147483648:
             return 0
 
-        # Check the upper limit
         if result > 2147483647:
             return 0
 
